fd00f69beffb1c237c160c7258688a00240cfd87
Young Liu <youngliu92@gmail.com>
2019-03-22 16:30:18 -0700

    add initial file structure.

---
+++ spec/ping-pong-spec.js
@@ -0,0 +1,14 @@
+import { AgeCalulator } from "./../src/ageCalculator.js"
+
+// describe('Ping Pong Ball', function() {
+//
+//   var reusablePingPong;
+//
+//   beforeEach(function(){
+//     reusablePingPong = new PingPongBall(5);
+//   });
+//
+//   it('should test whether a ping pong ball is created with the correct size', function() {
+//     expect(reusablePingPong.radius).toEqual(5);
+//   });
+// });
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+import { AgeCalulator } from './ageCalculator.js';
+
+$(document).ready(function() {
+  // $('#ping-pong-form').submit(function(event) {
+  //   event.preventDefault();
+  //   var goal = $('#goal').val();
+  //   var output = pingPong(goal);
+  //   output.forEach(function(element) {
+  //     $('#solution').append("<li>" + element + "</li>");
+  //   });
+  // });
+});
