4bbfa260b77b42748682fb33bd035fd707948ca4
Young Liu <youngliu92@gmail.com>
2019-03-23 11:45:37 -0700

    add logic for parsing input from form.

--- src/main.js
+++ src/main.js
@@ -2,15 +2,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { AgeCalulator } from './ageCalculator.js';
+import { AgeCalculator } from './ageCalculator.js';
 
 $(document).ready(function() {
   $('#galaxyForm').submit(function(event) {
     event.preventDefault();
-    // var goal = $('#goal').val();
-    // var output = pingPong(goal);
-    // output.forEach(function(element) {
-    //   $('#solution').append("<li>" + element + "</li>");
-    // });
+    let age = parseFloat($("#age").val());
+    let gender = $("input:radio[name=gender]:checked").val();
+    let excercise = parseInt($("#exerciseInput").val());
+    let badHabits = 0;
+    $("input:checkbox[name=habits]:checked").each(function(){
+      badHabits++;
+    });
+    let income = parseInt($("#incomeInput").val());
+    let ageCalculator = new AgeCalculator(age, gender, excercise, badHabits, income);
   });
 });
