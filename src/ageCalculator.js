317f29fb896587c2e0a6b28a00ea13dfdb89ca6d
Young Liu <youngliu92@gmail.com>
2019-03-22 21:21:51 -0700

    add backend for AgeCalculator.

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,19 +1,57 @@
-// export function pingPong(goal) {
-//   var output = [];
-//   for (var i = 1; i <= goal; i++) {
-//     if (i % 15 === 0) {
-//       output.push("ping-pong");
-//     } else if (i % 3 === 0) {
-//       output.push("ping");
-//     } else if (i % 5 === 0) {
-//       output.push("pong");
-//     } else  {
-//       output.push(i);
-//     }
-//   }
-//   return output;
-// }
-//
-// export function PingPongBall(radius){
-//   this.radius = radius;
-// }
+export class AgeCalculator{
+  constructor(age, gender, exercise, badHabits, income){
+    this.age = age;
+    this.gender = gender;
+    this.exercise = exercise;
+    this.badHabits = badHabits;
+    this.income = income;
+  }
+
+  convertMercury(age = 0){
+    const mercCoeff = .24;
+    if (age === 0){
+      return this.age / mercCoeff;
+    }
+    return age * mercCoeff;
+  }
+
+  convertVenus(age = 0){
+    const venusCoeff = .62;
+    if (age === 0){
+      return this.age / venusCoeff;
+    }
+    return age * venusCoeff;
+  }
+
+  convertMars(age = 0){
+    const marsCoeff = 1.88;
+    if (age === 0){
+      return this.age / marsCoeff;
+    }
+    return age * marsCoeff;
+  }
+
+  convertJupiter(age = 0){
+    const jupCoeff = 11.86;
+    if (age === 0){
+      return this.age / jupCoeff;
+    }
+    return age * jupCoeff;
+  }
+
+  getExpectancy(){
+    const averageAge = 78.7;
+    let ageMultiplier = 1;
+    if(this.gender === "female"){
+      ageMultiplier += .1;
+    }
+    else if(this.gender === "male"){
+      ageMultiplier -= .1;
+    }
+    ageMultiplier += this.exercise * .05;
+    ageMultiplier -= this.badHabits * .05;
+    ageMultiplier += income * .03;
+    let expectedAge = averageAge * ageMultiplier;
+    return expectedAge - this.age;
+  }
+}
